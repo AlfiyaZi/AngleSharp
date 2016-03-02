917c79e72549490ea0a75044257bdb6c25a4dd58
Florian Rappl <rappl.florian@gmail.com>
2016-03-02 23:48:41 +0100

    Rename to follow .NET conventions

--- AngleSharp/Dom/DomEventHandler.cs
+++ AngleSharp/Dom/DomEventHandler.cs
@@ -7,6 +7,6 @@
     /// Defines the callback signature for an event.
     /// </summary>
     /// <param name="sender">The callback this argument.</param>
-    /// <param name="ev">The event arguments.</param>
-    public delegate void DomEventHandler(Object sender, Event ev);
+    /// <param name="e">The event arguments.</param>
+    public delegate void DomEventHandler(Object sender, Event e);
 }
